f2f39a4483fdf63145815982f8b8179ba72b2f13
peche56 <c.filleul56@gmail.com>
2017-07-04 17:00:38 +0200

    event component hard

---
+++ src/components/Event.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Event extends Component {
+  render() {
+    return (
+      <div className="Event">
+          <p>titre</p>
+          <p>date</p>
+          <p>sport</p>
+          <p>places</p>
+      </div>
+    );
+  }
+}
+
+export default Event;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+// User from './components/User';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+
 );
